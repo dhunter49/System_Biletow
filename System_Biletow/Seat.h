e7d9110da45c179e17bea364c004d7a4a357f141
Adam <adammarzec2@protonmail.com>
2025-05-19 22:46:50 +0200

    Wprowadzenie tesowej bazy danych

--- System_Biletow/Seat.h
+++ System_Biletow/Seat.h
@@ -2,9 +2,12 @@
 
 class Seat
 {
+protected:
 	// variables
-	bool isFirstClass, isFacingFront, isByTable;
-	char locationOfSeating; // Where 'w' is window; 'm' is middle; 'c' is corridor
-	int seatNumber;
-	// bool isTaken[] // true if taken on station number
+	bool isFirstClass{}, isFacingFront{}, isByTable{}, isInCompartment{};
+	char locationOfSeating{}; // Where 'w' is window; 'm' is middle; 'c' is corridor
+	int seatNumber{};
+	// int takenBy[] // 0 if free, passengerID if taken on station number
+
+	// contructors
 };
--- System_Biletow/main.cpp
+++ System_Biletow/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "sqlite3.h"
 
 int main() {
-	std::cout << "Testowy cout";
+    sqlite3* db;
+    int rc = sqlite3_open("example.db", &db);
+
+    if (rc != SQLITE_OK) {
+        std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return 1;
+    }
+
+    // Use the database...
+
+    sqlite3_close(db);
 	return 0;
 }
