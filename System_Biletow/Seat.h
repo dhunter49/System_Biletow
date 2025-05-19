1948f57f0f0e2edbfeb5f521874040d050c2b5a6
Adam <adammarzec2@protonmail.com>
2025-05-19 20:48:01 +0200

    Dodanie SQLite oraz wpisanie w main testowego kodu otwierającego example.db

---
+++ System_Biletow/Seat.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Seat
+{
+	// variables
+	bool isFirstClass, isFacingFront, isByTable;
+	char locationOfSeating; // Where 'w' is window; 'm' is middle; 'c' is corridor
+	int seatNumber;
+	// bool isTaken[] // true if taken on station number
+};
