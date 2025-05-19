41965b736620e32b4fa49d22497cca5ef89cbced
mxrtaaa <marta.zaluska55@gmail.com>
2025-05-19 22:58:51 +0200

    Dodanie konstruktorów klasy Seat

---
+++ System_Biletow/Seat.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Seat //: protected car (przedzial)
+{
+protected:
+	// variables
+	bool isFirstClass{}, isFacingFront{}, isByTable{}, isInCompartment{};
+	char locationOfSeating{}; // Where 'w' is window; 'm' is middle; 'c' is corridor
+	int seatNumber{};
+	// int takenBy[] // 0 if free, passengerID if taken on station number
+
+	// constructors
+public:
+	Seat();
+	Seat(int);
+};
