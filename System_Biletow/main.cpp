06e4823cca83fe93c52dd2cdd9b6aaeda7249b6c
mxrtaaa <marta.zaluska55@gmail.com>
2025-05-24 23:45:48 +0200

    Added all tables and variables

--- System_Biletow/main.cpp
+++ System_Biletow/main.cpp
@@ -3,11 +3,25 @@
 
 int main() {
 	try {
-		SQLite::Database db("test_db.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+		SQLite::Database db("database.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+		SQLite::Statement query1(db, "SELECT * FROM Trains");
+		while (query1.executeStep()) {
+			std::cout << "ID: " << query1.getColumn(0) << "; ";
+			std::cout << "nazwa: " << query1.getColumn(1) << std::endl;
+		}
+		SQLite::Statement query(db, "SELECT * FROM TrainSets LEFT JOIN Trains ON TrainSets.TrainID = Trains.ID WHERE TrainSets.TripID IS NOT NULL");
+		while (query.executeStep()) {
+			std::cout << std::endl;
+			std::cout << "kurs: " << query.getColumn(0) << "; ";
+			std::cout << "ID: " << query.getColumn(1) << "; ";
+			std::cout << "wagon: " << query.getColumn(2) << std::endl << std::endl;
+
+		}
 	}
 	catch (const std::exception e) {
 		std::cerr << "Blad: " << e.what() << std::endl;
 	}
+
 	
 	return 0;
 }
