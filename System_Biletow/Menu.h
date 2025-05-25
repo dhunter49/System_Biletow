aedf174919ed9569135010ae6a7a297c8052bfe7
Adam Marzec <adammarzec2@protonmail.com>
2025-05-25 14:05:35 +0200

    Stworzona fukcja menu - z testem w main

---
+++ System_Biletow/Menu.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string> 
+#include <vector>
+
+int showMenu(std::string, const std::vector <std::string>);
--- System_Biletow/main.cpp
+++ System_Biletow/main.cpp
@@ -1,26 +1,41 @@
 #include "headers.h"
 #include <SQLiteCpp/SQLiteCpp.h>
+#include "Menu.h"
 
 int main() {
-	try {
-		SQLite::Database db("database.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
-		SQLite::Statement query1(db, "SELECT * FROM Trains");
-		while (query1.executeStep()) {
-			std::cout << "ID: " << query1.getColumn(0) << "; ";
-			std::cout << "nazwa: " << query1.getColumn(1) << std::endl;
-		}
-		SQLite::Statement query(db, "SELECT * FROM TrainSets LEFT JOIN Trains ON TrainSets.TrainID = Trains.ID WHERE TrainSets.TripID IS NOT NULL");
-		while (query.executeStep()) {
-			std::cout << std::endl;
-			std::cout << "kurs: " << query.getColumn(0) << "; ";
-			std::cout << "ID: " << query.getColumn(1) << "; ";
-			std::cout << "wagon: " << query.getColumn(2) << std::endl << std::endl;
+	//try {
+	//	SQLite::Database db("database.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+	//	SQLite::Statement query1(db, "SELECT * FROM Trains");
+	//	while (query1.executeStep()) {
+	//		std::cout << "ID: " << query1.getColumn(0) << "; ";
+	//		std::cout << "nazwa: " << query1.getColumn(1) << std::endl;
+	//	}
+	//	SQLite::Statement query(db, "SELECT * FROM TrainSets LEFT JOIN Trains ON TrainSets.TrainID = Trains.ID WHERE TrainSets.TripID IS NOT NULL");
+	//	while (query.executeStep()) {
+	//		std::cout << std::endl;
+	//		std::cout << "kurs: " << query.getColumn(0) << "; ";
+	//		std::cout << "ID: " << query.getColumn(1) << "; ";
+	//		std::cout << "wagon: " << query.getColumn(2) << std::endl << std::endl;
 
-		}
+	//	}
+	//}
+	//catch (const std::exception e) {
+	//	std::cerr << "Blad: " << e.what() << std::endl;
+	//}
+	std::vector<std::string> mainMenuOptions = {
+		"1. test1", "2. test 2"
+	};
+	try {
+		std::cout << showMenu("Glowne", mainMenuOptions);
 	}
 	catch (const std::exception e) {
-		std::cerr << "Blad: " << e.what() << std::endl;
+		std::cerr << "Error: " << e.what() << std::endl;
 	}
+	catch (...) {
+		std::cerr << "Error: unknown" << std::endl;
+	}
+
+
 
 	
 	return 0;
