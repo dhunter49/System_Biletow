5c766035eb75fc456116ea5e0bbce2291b2071e9
Adam <adammarzec2@protonmail.com>
2025-05-27 19:13:45 +0200

    Added support for routes not starting with 1 as ID; StationMap now is correctly added to routes (is cleared); Only stations that are not hidden are imported

--- System_Biletow/Route.cpp
+++ System_Biletow/Route.cpp
@@ -17,23 +17,29 @@ Route::Route(int routeID, std::unordered_map<int, Station> listOfStations) :rout
 void RoutesManager::loadRoutesFromDatabase() {
 	try {
 		SQLite::Database db(DATABASE_PATH, SQLite::OPEN_READONLY);
-		SQLite::Statement query(db, "SELECT ID, StationNumber, StationID FROM Routes");
+		SQLite::Statement query(db, "SELECT ID, StationNumber, StationID FROM Routes WHERE IsShowing = 1");
 
-		int id = 1;
 		int stationID{}; // Station unique ID
 		int stationNum{}; // Station number in list, always starts at 1, for every route
 		std::unordered_map<int, Station> listOfStations;
-		while (query.executeStep()) {
+
+		if (!query.executeStep())
+			throw std::runtime_error("No routes found in database!");
+		int id = query.getColumn(0).getInt();
+
+		do {
 			if (id != query.getColumn(0).getInt()) {
 				Route route(id, listOfStations);
 				routes[id] = route;
 				id = query.getColumn(0).getInt();
+				listOfStations.clear();
 			}
 			stationNum = query.getColumn(1).getInt();
 			stationID = query.getColumn(2).getInt();
 			Station station = findInDatabase(stationID);
 			listOfStations[stationNum] = { stationID, station.name };
-		}
+		} while (query.executeStep());
+
 		Route route(id, listOfStations);
 		routes[id] = route;
 	}
