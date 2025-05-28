f4f181a20bbe8d3159e524461ab1bd7bab77c600
Adam <adammarzec2@protonmail.com>
2025-05-28 20:03:01 +0200

    Added initialization of all other schedules after the first station

--- System_Biletow/Trip.cpp
+++ System_Biletow/Trip.cpp
@@ -1,4 +1,4 @@
-#include "Trip.h"
+﻿#include "Trip.h"
 #include "GlobalConsts.h"
 #include <SQLiteCpp/SQLiteCpp.h>
 
@@ -22,7 +22,7 @@ void initializeTripsByDate(Date date, int routeID) {
     query.bind(3, date.month < 10 ? "0" + std::to_string(date.month) : std::to_string(date.month)); // Ensure 2-digit format
     query.bind(4, std::to_string(date.year));
     if (!query.executeStep())
-        throw std::runtime_error("Brak przejazd�w danego dnia tej relacji!");
+        throw std::runtime_error("Brak przejazdów danego dnia tej relacji!");
     int currentTripID = query.getColumn(0).getInt();
     Trip currentTrip;
     do {
@@ -36,6 +36,38 @@ void initializeTripsByDate(Date date, int routeID) {
         };        
         trips.push_back(currentTrip);
     } while (query.executeStep());
+
+    for (auto& itTrip : trips) {
+        itTrip.initializeAllOtherSchedules();
+    }
+}
+
+void Trip::initializeAllOtherSchedules() {
+    SQLite::Database db(DATABASE_PATH, SQLite::OPEN_READONLY);
+    SQLite::Statement query(db, "SELECT StationNumber, StationID, ArrivalTime, DepartureTime FROM Routes WHERE ID = ? ORDER BY StationNumber");
+    query.bind(1, routeID);
+
+    if (!query.executeStep())
+        throw std::runtime_error("Niespodziewany błąd! Wczytany przejazd nie istnieje w relacjach!");
+
+    unsigned int hoursArrival{}, minutesArrival{}, hoursDeparture{}, minutesDeparture{}, additionalMinutesArrival{}, additionalMinutesDeparture{};
+    int stationNum{}, stationID{};
+    while (query.executeStep()) {
+        stationNum = query.getColumn(0).getInt();
+        stationID = query.getColumn(1).getInt();
+        additionalMinutesArrival = query.getColumn(2).getUInt();
+        additionalMinutesDeparture = query.getColumn(3).getUInt();
+        minutesArrival = (schedules[1].arrival.minutes + additionalMinutesArrival)%60;
+        hoursArrival = schedules[1].arrival.hours + additionalMinutesArrival / 60;
+        minutesDeparture = (schedules[1].departure.minutes + additionalMinutesDeparture) % 60;
+        hoursDeparture = schedules[1].departure.hours + additionalMinutesDeparture / 60;
+
+        schedules[stationNum] = Schedule{
+            stationID,
+            Time{hoursArrival, minutesArrival},
+            Time{hoursDeparture, minutesDeparture}
+        };
+    }
 }
 
 Trip::Trip() :tripID(0) {};
