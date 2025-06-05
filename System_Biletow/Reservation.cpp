7ff2d403dc3180e2532790fa757b4907d8751337
Adam <adammarzec2@protonmail.com>
2025-06-05 23:09:03 +0200

    Dodano klasę Reservation oraz szkic metody szukającej wolnego miejsca

---
+++ System_Biletow/Reservation.cpp
@@ -0,0 +1,51 @@
+﻿#include "Reservation.h"
+#include "DataManager.h"
+
+bool Reservation::findASeat() {
+	auto& data = DataManager::getInstance();
+	// check if there is enough spots in the entire train
+	if (data.currentTrain.getFreeSeats(fromStationNumber, toStationNumber) != 0) {
+		data.getCarsByTrainID(data.currentTrain.getTrainID());
+		for (auto& carPair : data.currentCars) {
+			// check if there is enough spots in a car
+			if (carPair.getFreeSeats(fromStationNumber, toStationNumber) != 0) {
+				data.getCompartmentsByCarNumber(carPair.getCarNumber());
+				for (auto& compartmentPair : data.currentCompartments) {
+					// check if there is enough spots in a compartment
+					if (compartmentPair.getFreeSeats(fromStationNumber, toStationNumber) != 0) {
+						data.getSeatsByCompartmentNumber(compartmentPair.getCompartmentNumber(), carPair.getCarNumber());
+						for (auto& seatPair : data.currentSeats) {
+							// check if seat meets preferences
+							if (meetsPreferences(seatPair)) {
+								carNumber = carPair.getCarNumber();
+								seatNumber = seatPair.getSeatNumber();
+								tripID = seatPair.getTripID();
+								//ticketPrice = getPrice();
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+		std::vector<MenuOption> yesOrNo;
+		yesOrNo.push_back(MenuOption{ 1, "Tak" });
+		yesOrNo.push_back(MenuOption{ 0, "Nie" });
+		showMenu("Pociąg nie ma miejsc spełniającyh preferecje, lecz są dostępne miejsca. Czy chcesz wyczyścić preferencje?", yesOrNo);
+	}
+	else {
+		std::cout << "Pociąg nie ma wolnych miejsc!\n";
+		return false;
+	}
+}
+
+bool Reservation::meetsPreferences(Seat seat) {
+	if (!facingFront.isChosen || facingFront.value == seat.getIsFacingFront()
+		&& !byTable.isChosen || byTable.value == seat.getIsByTable()
+		&& !window.isChosen || window.value == seat.getIsWindow()
+		&& !middle.isChosen || middle.value == seat.getIsMiddle()
+		&& !corridor.isChosen || corridor.value == seat.getIsCorridor()) {
+		return true;
+	}
+	return false;
+}
---
+++ System_Biletow/Reservation.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+
+struct Preference {
+	bool isChosen;
+	bool value;
+};
+
+class Reservation {
+private:
+	// before reservation
+	int fromStationNumber{};
+	int toStationNumber{};
+	bool firstClass{};
+	Preference facingFront{};
+	Preference byTable{};
+	Preference window{};
+	Preference middle{};
+	Preference corridor{};
+	std::string firstName{};
+	std::string lastName{};
+
+	// after reservation
+	int carNumber{};
+	int seatNumber{};
+	int tripID{};
+	float ticketPrice{};
+public:
+	bool findASeat();
+	bool meetsPreferences(Seat seat);
+};
+
