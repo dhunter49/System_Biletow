1cf2bff7d163ac2020eadf95ce5c5c9c3bea9c85
mxrtaaa <marta.zaluska55@gmail.com>
2025-06-06 10:00:28 +0200

    Changed file structure for Menu

--- System_Biletow/Menu.h
+++ System_Biletow/Menu.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <string> 
 #include <vector>
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <iomanip>
 
 struct MenuOption {
     int id;
     std::string menuText;
 };
+
+
+void setColor(int color);
+void getConsoleDimensions(int& lines, int& columns);
+void setConsoleCursorVisibility(bool visible);
+void clearScreen();
+void gotoXY(int x, int y);
 
 int showMenu(std::string, const std::vector<MenuOption>);
